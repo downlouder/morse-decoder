7ace22e3a0c13cc0a7ce3699ea7a38913947f35b
Nazarenko Yurii <wallpapeforpc@gmail.com>
2023-07-27 20:33:20 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0, j = 10; j <= expr.length; i += 10, j += 10) {
+        const charBit = expr.slice(i, j);
+        let morse = '';
+        if (charBit === '**********')
+            morse = ' ';
+        else {
+            for (let a = 0, b = 2; b <= 10; a += 2, b += 2) {
+                const charCode = charBit.slice(a, b);
+                if (charCode === '10') 
+                    morse += '.';
+                else if (charCode === '11') 
+                    morse += '-';
+            }
+        }
+        result += (morse === ' ') ? morse : MORSE_TABLE[morse];
+    }    
+    return result;
 }
 
 module.exports = {
